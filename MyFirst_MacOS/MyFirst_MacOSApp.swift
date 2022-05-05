846da6f5df5f5aa786f3299509a474a225bc7444
Huy Le <lehuydit@gmail.com>
2022-05-05 16:04:32 +0700

    Initial Commit

---
+++ MyFirst_MacOS/MyFirst_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirst_MacOSApp.swift
+//  MyFirst_MacOS
+//
+//  Created by LE CONG HUY on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyFirst_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
